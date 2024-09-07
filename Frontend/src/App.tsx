a8c3e63da49ef540df8dbdc1ded52920c838ec59
Edison <aalevenegass@gmail.com>
2024-09-06 21:51:25 -0400

    Ajustando colores de navbar modo claro y oscuro

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 
 function App(): JSX.Element {
   return (
-    <div className="app__container">
+    <div className="app__container min-h-[100vh] bg-[#EBEBEB] dark:bg-[#22264B]">
       <Navbar />
     </div>
   );
--- Frontend/src/components/navbar/Navbar.tsx
+++ Frontend/src/components/navbar/Navbar.tsx
@@ -1,8 +1,14 @@
+import { FaCircleUser } from "react-icons/fa6";
+
 function Navbar(): JSX.Element {
   return (
-    <div className="">
-      <h1>Hola soy el navbar</h1>
-    </div>
+    <section className="bg-white dark:bg-[#393C73] p-2">
+      <div>
+        <div>
+          <FaCircleUser className="text-[30px] text-teal-500 dark:text-slate-400" />
+        </div>
+      </div>
+    </section>
   );
 }
 
