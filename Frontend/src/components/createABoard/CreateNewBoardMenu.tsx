aac15c29168513ab5439257cf02b86a8516c1de2
Edison <aalevenegass@gmail.com>
2024-09-07 18:12:09 -0400

    Añadiendo imágenes en el menu de creación de tablero

--- Frontend/src/components/createABoard/CreateNewBoardMenu.tsx
+++ Frontend/src/components/createABoard/CreateNewBoardMenu.tsx
@@ -2,6 +2,9 @@ import { motion } from "framer-motion";
 import { forwardRef, useRef, useEffect } from "react";
 import { OpenCreateBoardMenu } from "../../types/ThemeMenuProp";
 import { IoClose } from "react-icons/io5";
+import board1 from "/images/application/boards-background/board-1.png";
+import skeleton from "/images/application/boards-background/skeleton.png";
+import { background } from "./background.ts";
 
 const CreateNewBoardMenu = forwardRef<HTMLDivElement, OpenCreateBoardMenu>(
   ({ setOpenCreateBoardMenu, setOpenModal }, _) => {
@@ -38,22 +41,48 @@ const CreateNewBoardMenu = forwardRef<HTMLDivElement, OpenCreateBoardMenu>(
           exit={{ opacity: 0, scale: 0.9 }}
           transition={{ duration: 0.2, ease: "backInOut" }}
           style={{ originX: 1, originY: 0 }}
-          className="absolute top-[45px] right-0 w-[250px] bg-white dark:bg-[#393C73] rounded-md shadow-lg p-2"
+          className="absolute top-[45px] right-0 w-[320px] bg-white dark:bg-[#393C73] rounded-md shadow-lg p-2"
         >
           <div className="p-1 rounded select-none">
-            <div className="flex items-center justify-center gap-2 mb-2 relative">
+            <div className="flex items-center justify-center gap-2 mb-5 relative">
               <h1 className="font-medium text-slate-800 dark:text-white">
                 Crear tablero
               </h1>
               <IoClose
                 onClick={handleCloseBoardMenu}
-                className="text-[28px] text-slate-800 dark:text-white cursor-pointer absolute top-0 right-0 hover:bg-[#0000002a] rounded-md p-1"
+                className="text-[28px] text-slate-800 dark:text-white cursor-pointer absolute top-[-2px] right-0 hover:bg-[#0000002a] rounded-md p-1"
               />
             </div>
-            <span className="text-xs font-normal text-slate-800 dark:text-white">
-              Un tablero es un conjunto de tarjetas ordenadas como lista, úsalo
-              para gestionar tus tareas diarias, o proyectos.
-            </span>
+            <div className="flex items-center justify-center mb-4">
+              <figure className="relative">
+                <img
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[160px]"
+                  src={skeleton}
+                  alt=""
+                />
+
+                <img
+                  className="rounded shadow max-w-[200px]"
+                  src={board1}
+                  alt="fondo-tablero-1"
+                />
+              </figure>
+            </div>
+            <div>
+              <h1 className="text-slate-800 dark:text-white font-medium text-sm mb-1">
+                Fondos
+              </h1>
+              <div className="flex items-center justify-between gap-1">
+                {background.map((img) => (
+                  <img
+                    key={img.id}
+                    className="rounded w-full max-w-[68px] h-[40px] object-fill cursor-pointer hover:brightness-125 hover:outline outline-1 outline-slate-600 hover:dark:outline-white"
+                    src={img.href}
+                    alt="fondo-tablero-1"
+                  />
+                ))}
+              </div>
+            </div>
           </div>
         </motion.div>
       </div>
---
+++ Frontend/src/components/createABoard/background.ts
@@ -0,0 +1,28 @@
+
+interface BackgroundModel {
+  id: number,
+  href: string
+
+
+}
+
+
+export const background: BackgroundModel[] = [
+  {
+    "id": 1,
+    "href": "/images/application/boards-background/board-1.png"
+  },
+  {
+    "id": 2,
+    "href": "/images/application/boards-background/board-2.jpeg"
+  },
+  {
+    "id": 3,
+    "href": "/images/application/boards-background/board-3.jpg"
+  },
+  {
+    "id": 4,
+    "href": "/images/application/boards-background/board-4.jpg"
+  }
+]
+
