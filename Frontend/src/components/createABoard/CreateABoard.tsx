1735706cf104b6175d38ad9c36ed5693f1a489b5
Edison <aalevenegass@gmail.com>
2024-09-07 14:52:10 -0400

    Creando función para manejo de apertura y cierre de modal

--- Frontend/src/components/createABoard/CreateABoard.tsx
+++ Frontend/src/components/createABoard/CreateABoard.tsx
@@ -1,13 +1,51 @@
 import { motion } from "framer-motion";
+import CreateABoardModal from "./CreateABoardModal";
+import { useEffect, useRef, useState } from "react";
+import { DomRefElement } from "../../types/DomRefElement";
 
 function CreateABoard() {
+  const [openModal, setOpenModal] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const modalRef = useRef<DomRefElement>(null);
+
+  const handleOpenBoardModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      buttonRef.current &&
+      modalRef.current &&
+      !buttonRef.current.contains(event.target as Node) &&
+      !modalRef.current.contains(event.target as Node)
+    ) {
+      setOpenModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <motion.section
-      whileTap={{ scale: 0.92 }}
-      className="bg-teal-300 rounded shadow dark:bg-[#a0a0b9] hover:brightness-110"
-    >
-      <button className="text-slate-700 font-medium py-1 px-3 ">Crear</button>
-    </motion.section>
+    <div className="relative">
+      <motion.section
+        whileTap={{ scale: 0.92 }}
+        className="bg-teal-300 rounded shadow dark:bg-[#a0a0b9] hover:brightness-110"
+      >
+        <button
+          ref={buttonRef}
+          onClick={handleOpenBoardModal}
+          className="text-slate-700 font-medium py-1 px-3 "
+        >
+          Crear
+        </button>
+      </motion.section>
+      {openModal ? <CreateABoardModal ref={modalRef} /> : null}
+    </div>
   );
 }
 
--- Frontend/src/components/createABoard/CreateABoardModal.tsx
+++ Frontend/src/components/createABoard/CreateABoardModal.tsx
@@ -1,9 +1,26 @@
-function CreateABoardModal() {
+import { forwardRef } from "react";
+import { TbLayoutBoard } from "react-icons/tb";
+
+const CreateABoardModal = forwardRef<HTMLDivElement>((_, ref) => {
   return (
-    <div>
-      <h1>Hola po loco</h1>
+    <div
+      ref={ref}
+      className="absolute top-[45px] right-0 w-[250px] bg-white dark:bg-[#393C73] rounded shadow-lg p-2"
+    >
+      <div className="cursor-pointer hover:bg-[#00000015] p-1 rounded select-none">
+        <div className="flex items-center gap-2 mb-2">
+          <TbLayoutBoard className="text-[20px] text-slate-800 dark:text-white" />
+          <h1 className="text-sm text-slate-800 dark:text-white">
+            Crea un tablero
+          </h1>
+        </div>
+        <span className="text-xs font-normal text-slate-800 dark:text-white">
+          Un tablero es un conjunto de tarjetas ordenadas como lista, úsalo para
+          gestionar tus tareas diarias, o proyectos.
+        </span>
+      </div>
     </div>
   );
-}
+});
 
 export default CreateABoardModal;
