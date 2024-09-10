cfdd2b0de4fafeb1d8d07109f9e96f8bb1fcfade
Edison <aalevenegass@gmail.com>
2024-09-10 18:29:06 -0300

    Creando función para almacenar el valor del nombre de la tarjeta

--- Frontend/src/components/taskList/TaskList.tsx
+++ Frontend/src/components/taskList/TaskList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CgMathPlus } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
+import { ChangeEvent } from "react";
 
 export interface taskInterface {
   id: string;
@@ -8,6 +9,8 @@ export interface taskInterface {
 }
 
 const TaskList = ({ title, id }: taskInterface) => {
+  const [taskListId, setTaskListId] = useState("");
+  const [taskTitle, setTaskTitle] = useState("");
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -16,14 +19,27 @@ const TaskList = ({ title, id }: taskInterface) => {
     },
   ]);
 
-  const handleAddNewTask = () => {};
+  const handleAddNewTask = (id: string) => {
+    if (id) {
+      setTaskListId(id);
+    } else {
+      setTaskListId("");
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTaskTitle(e.target.value);
+  };
 
   return (
-    <div id={id} className="bg-[#1b1b1b] w-[300px] px-3 py-3 rounded-2xl">
+    <div
+      id={id}
+      className="bg-[#1b1b1b] w-[300px] px-3 py-3 rounded-2xl shadow-lg"
+    >
       <h1 className="text-slate-800 dark:text-gray-300 mb-2 px-5 font-medium">
         {title}
       </h1>
-      <div className="my-2 flex flex-col gap-3">
+      <div className="flex flex-col gap-3">
         {id === "1"
           ? tasks.map((task) => (
               <div
@@ -37,31 +53,45 @@ const TaskList = ({ title, id }: taskInterface) => {
             ))
           : null}
         <div>
-          {id === "1" ? (
+          {id === taskListId ? (
             <div>
               <div className="px-2 bg-slate-800 relative rounded">
                 <textarea
+                  onChange={handleChange}
+                  value={taskTitle}
                   name="task"
                   className="bg-transparent border-none text-slate-800 dark:text-gray-300 w-full outline-none resize-none"
                 />
-                <p className="text-slate-800 dark:text-gray-300 text-sm font-normal absolute top-[3px] left-[9px] pointer-events-none">
-                  Introduce un nombre para esta tarjeta...
-                </p>
+                {!taskTitle ? (
+                  <p className="text-slate-800 dark:text-gray-300 text-sm font-normal absolute top-[3px] left-[9px] pointer-events-none">
+                    Introduce un nombre para esta tarjeta...
+                  </p>
+                ) : null}
               </div>
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-1">
                 <button className="bg-[#383e9e] text-slate-800 dark:text-gray-300 px-2 py-[6px] rounded shadow text-sm font-medium my-2 hover:brightness-125">
                   Añadir tarjeta
                 </button>
-                <IoClose className="text-slate-800 dark:text-gray-300 cursor-pointer text-[35px] hover:bg-[#333333] rounded-full p-1" />
+                <IoClose
+                  onClick={() => setTaskListId("")}
+                  className="text-slate-800 dark:text-gray-300 cursor-pointer text-[30px] hover:bg-[#333333] rounded-full p-1"
+                />
               </div>
             </div>
           ) : null}
         </div>
       </div>
-      <div className="flex items-center gap-2 hover:bg-[#96969663] cursor-pointer py-1 px-2 rounded duration-300">
-        <CgMathPlus className="text-slate-800 dark:text-gray-300 text-[20px]" />
-        <p className="text-slate-800 dark:text-gray-300">Añadir una tarjeta</p>
-      </div>
+      {taskListId === id ? null : (
+        <div
+          onClick={() => handleAddNewTask(id)}
+          className="flex items-center gap-2 hover:bg-[#96969663] cursor-pointer py-1 px-2 rounded duration-300"
+        >
+          <CgMathPlus className="text-slate-800 dark:text-gray-300 text-[20px]" />
+          <button className="text-slate-800 dark:text-gray-300">
+            Añadir una tarjeta
+          </button>
+        </div>
+      )}
     </div>
   );
 };
