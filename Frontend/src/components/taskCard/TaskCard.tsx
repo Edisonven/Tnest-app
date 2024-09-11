301c4c56324687f1e38929dc1b7e87d991d181c4
Edison <aalevenegass@gmail.com>
2024-09-11 20:19:22 -0300

    Creando modal para complementar opciones de tareas

--- Frontend/src/components/taskCard/TaskCard.tsx
+++ Frontend/src/components/taskCard/TaskCard.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import TaskCardOptions from "./TaskCardOptions";
+import { AnimatePresence } from "framer-motion";
+
 export interface TaskIterface {
   id: number;
   title: string;
@@ -5,12 +9,26 @@ export interface TaskIterface {
 }
 
 function TaskCard({ id, title }: TaskIterface) {
+  const [openTaskOptions, setOpenTaskOptions] = useState(false);
+
+  const handleOpenTaskOptions = (): void => {
+    setOpenTaskOptions(!openTaskOptions);
+  };
+
   return (
-    <div
-      key={id}
-      className="bg-slate-800 px-3 py-[6px] rounded cursor-pointer hover:brightness-125 hover:outline outline-1 outline-white "
-    >
-      <p className="text-slate-800 dark:text-gray-300 font-normal">{title}</p>
+    <div>
+      <div
+        onClick={handleOpenTaskOptions}
+        key={id}
+        className="bg-slate-800 px-3 py-[6px] rounded cursor-pointer hover:brightness-125 hover:outline outline-1 outline-white"
+      >
+        <p className="text-slate-800 dark:text-gray-300 font-normal">{title}</p>
+      </div>
+      <AnimatePresence>
+        {openTaskOptions ? (
+          <TaskCardOptions setOpenTaskOptions={setOpenTaskOptions} />
+        ) : null}
+      </AnimatePresence>
     </div>
   );
 }
--- Frontend/src/components/taskCard/TaskCardOptions.tsx
+++ Frontend/src/components/taskCard/TaskCardOptions.tsx
@@ -1,9 +1,22 @@
-function TaskCardOptions() {
-  return (
-    <div>
-      <h1>TaskCardOptions</h1>
-    </div>
-  );
+import { IoCloseOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
+
+interface TaskCardOptionsProps {
+  setOpenTaskOptions: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+const TaskCardOptions: React.FC<TaskCardOptionsProps> = ({
+  setOpenTaskOptions,
+}) => {
+  return (
+    <motion.div className="bg-[#25334A] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[600px] h-[500px] rounded shadow outline outline-1 outline-gray-700 p-4">
+      <IoCloseOutline
+        onClick={() => setOpenTaskOptions(false)}
+        className="absolute top-[8px] right-[8px] text-slate-800 dark:text-gray-300 text-[40px] cursor-pointer p-1 hover:bg-[#b4b4b42c] rounded-md duration-200"
+      />
+      <h1 className="text-slate-800 dark:text-gray-300">TaskCardOptions</h1>
+    </motion.div>
+  );
+};
 
 export default TaskCardOptions;
