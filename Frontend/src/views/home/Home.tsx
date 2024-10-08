1f692bff83f7063f90dd7c3559f01edfafdc18b4
Edison <aalevenegass@gmail.com>
2024-10-08 17:31:07 -0300

    Instalando librería material ui

--- Frontend/src/views/home/Home.tsx
+++ Frontend/src/views/home/Home.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import background_1 from "/images/application/background-1.svg";
 import { CreateBoardMenuContext } from "../../context/CreateBoardContext";
+import Button from "@mui/material/Button";
 
 function Home(): JSX.Element {
   const { setOpenCreateBoardMenu } = useContext(CreateBoardMenuContext);
@@ -16,12 +17,29 @@ function Home(): JSX.Element {
             Crea un tablero y comienza a trabajar
           </p>
           <div className="mt-6">
-            <button
+            {/*  <button
               onClick={() => setOpenCreateBoardMenu(true)}
               className="text-slate-800 dark:text-gray-300 px-3 py-2 bg-black shadow rounded-3xl font-medium"
             >
               Crear Tablero
-            </button>
+            </button> */}
+            <Button
+              onClick={() => setOpenCreateBoardMenu(true)}
+              sx={{
+                borderRadius: 30,
+                fontWeight: 700,
+                backgroundColor: "#ffffff",
+                height: 45,
+                color: "#141826",
+                "&:hover": {
+                  filter: "brightness(70%)",
+                },
+                transition: "all 0.3s ease",
+              }}
+              variant="contained"
+            >
+              Crear Tablero
+            </Button>
           </div>
         </div>
         <div>
