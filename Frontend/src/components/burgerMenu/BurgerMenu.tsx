1074199ea949c7eb7c661656ea47e69d450255c6
Edison <aalevenegass@gmail.com>
2024-11-29 11:23:25 -0300

    Añadiendo logo en menu perfil

--- Frontend/src/components/burgerMenu/BurgerMenu.tsx
+++ Frontend/src/components/burgerMenu/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { forwardRef } from "react";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
 
 const BurgerMenu = forwardRef<HTMLDivElement>(({}, ref) => {
   return (
@@ -13,16 +14,16 @@ const BurgerMenu = forwardRef<HTMLDivElement>(({}, ref) => {
       style={{ originX: 1, originY: 0 }}
       className="bg-white shadow-lg rounded dark:bg-[#141826] p-3 absolute top-[44px] right-0 w-[150px] outline outline-1 outline-gray-800"
     >
-      <div className="flex flex-col items-center gap-3 w-full">
-        
-      </div>
+      <div className="flex flex-col items-center gap-3 w-full"></div>
       <hr className="my-2" />
       <Link
-        className="text-slate-800 dark:text-white w-full text-center text-sm font-medium"
+        className="text-slate-800 dark:text-white w-full text-center text-sm font-medium flex items-center justify-center gap-2"
         to="/my-board"
       >
         Mis tableros
+        <MdOutlineSpaceDashboard className="text-[22px]" />
       </Link>
+      <hr className="my-2" />
     </motion.div>
   );
 });
